42e5fe80b3d6fae4657ab7283ed6230c97334de6
erikvw <ew2789@gmail.com>
2017-08-01 16:54:57 +0200

    add anonymous subject type to enrollment cap

--- bcpp_consent/apps.py
+++ bcpp_consent/apps.py
@@ -19,7 +19,9 @@ if settings.APP_NAME == 'bcpp_consent':
         protocol_title = 'Botswana Combination Prevention Project'
         subject_types = [
             SubjectType('subject', 'Research Subject',
-                        Cap(model_name='bcpp_subject.subjectconsent', max_subjects=9999)),
+                        Cap(model_name='bcpp_subject.subjectconsent', max_subjects=99999)),
+            SubjectType('subject', 'Anonymous Research Subject',
+                        Cap(model_name='bcpp_subject.anonymousconsent', max_subjects=9999)),
         ]
         study_open_datetime = datetime(
             2013, 10, 18, 0, 0, 0, tzinfo=gettz('UTC'))
